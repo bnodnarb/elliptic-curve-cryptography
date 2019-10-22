2817e766980fb96a5dacee8c4792e569426a7d07
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-22 00:40:37 -0700

    both private keys

--- scripts/generate/ecdh.js
+++ scripts/generate/ecdh.js
@@ -1,13 +1,14 @@
 var EC = require('elliptic').ec;
 var ec = new EC('curve25519');
 
-// Generate keys
-var key1 = ec.genKeyPair();
-var key2 = ec.genKeyPair();
+var key1 = ec.keyFromPrivate('49c0c4a5647e43cb381b71a96d90c962a79353f386cbe2316e7f57dd64fe81aa');
+var publicKey1 = key1.getPublic();
 
-var shared1 = key1.derive(key2.getPublic());
-var shared2 = key2.derive(key1.getPublic());
+var key2 = ec.keyFromPrivate('9d0dbf885862123960795c21c2134262b24e69610ba189692b8619b6142e7a66');
+var publicKey2 = key2.getPublic();
 
-console.log('Both shared secrets are BN instances');
+var shared1 = key1.derive(publicKey2);
+var shared2 = key2.derive(publicKey1);
+
 console.log(shared1.toString(16));
 console.log(shared2.toString(16));
