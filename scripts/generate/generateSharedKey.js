154bf87e487afbd223fe0af77d7f4d6052008827
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-22 13:18:19 -0700

    using key pair

--- scripts/generate/generateSharedKey.js
+++ scripts/generate/generateSharedKey.js
@@ -1,16 +1,13 @@
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
-var SHA256 = require('crypto-js/sha256');
 var argv = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 
-collaboratorPublicKey = argv.collaboratorPublicKey;
-privatePhrase = argv.privatePhrase;
-sha256PrivatePhrase = SHA256(privatePhrase).toString();
+privateKey = argv.privateKey;
+publicKey = argv.publicKey;
 
-var key = ec.keyFromPrivate(sha256PrivatePhrase);
-var publicKey = key.getPublic('hex');
-var privateKey = key.getPrivate('hex');
+var key = ec.keyFromPrivate(privateKey);
 
-var sharedKey = key.derive(ec.keyFromPublic(collaboratorPublicKey, 'hex').getPublic());
+var sharedKey = key.derive(ec.keyFromPublic(publicKey, 'hex').getPublic());
+
 console.log(chalk.red(chalk.bold('sharedKey: ') + sharedKey.toString(16)));
