0ff90189211fc6f64fca4d1834ce33b2647662eb
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-22 15:47:20 -0700

    spacing

--- scripts/generate/generateKeyPairPrivatePhrase.js
+++ scripts/generate/generateKeyPairPrivatePhrase.js
@@ -11,8 +11,7 @@ var key = ec.keyFromPrivate(sha256PrivatePhrase);
 var publicKey = key.getPublic('hex');
 var privateKey = key.getPrivate('hex');
 
-console.log(chalk.grey(chalk.bold('privatePhrase: ') + privatePhrase));
-console.log(chalk.grey(chalk.bold('SHA256 of privatePhrase: ') + sha256PrivatePhrase));
 console.log();
 console.log(chalk.red(chalk.bold('privateKey: ') + privateKey));
 console.log(chalk.green(chalk.bold('publicKey: ') + publicKey));
+console.log();
