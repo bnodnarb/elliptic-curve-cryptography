e7a411d68ffcf9b3e50934d3dac876a44211f358
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-23 11:25:17 -0700

    change the params

--- scripts/generate/generateSharedKey.js
+++ scripts/generate/generateSharedKey.js
@@ -3,11 +3,11 @@ var ec = new EC('secp256k1');
 var argv = require('minimist')(process.argv.slice(2));
 const chalk = require('chalk');
 
-privateKey = argv.privateKey;
-publicKey = argv.publicKey;
+myPrivateKey = argv.myPrivateKey;
+collaboratorPublicKey = argv.collaboratorPublicKey;
 
-var key = ec.keyFromPrivate(privateKey);
+var key = ec.keyFromPrivate(myPrivateKey);
 
-var sharedKey = key.derive(ec.keyFromPublic(publicKey, 'hex').getPublic());
+var sharedKey = key.derive(ec.keyFromPublic(collaboratorPublicKey, 'hex').getPublic());
 
 console.log(chalk.red(chalk.bold('sharedKey: ') + sharedKey.toString(16)));
