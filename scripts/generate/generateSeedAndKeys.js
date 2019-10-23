950c61804facb1aa1b89508dfeaaa698e1be68ae
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-23 15:37:21 -0700

    simplified

--- scripts/generate/generateSeedAndKeys.js
+++ scripts/generate/generateSeedAndKeys.js
@@ -1,27 +1,15 @@
 var functions = require("../functions.js");
 var EC = require('elliptic').ec;
 var ec = new EC('secp256k1');
-const fs = require('fs');
-const readline = require('readline');
-const chalk = require('chalk');
 
-const seedList = [];
+var seedList = functions.generateSeed();
 
-var wordList = functions.getBip39WordList();
+sha256SeedList = functions.sha256(seedList);
 
-for (i = 1; i <= 12; i++) {
-  var randomInteger = functions.getRandomInteger(0,2047);
-  var word = wordList[randomInteger];
-  seedList.push(word);
-}
-var dashSeedList = seedList.join('-')
-
-sha256DashSeedList = functions.sha256(dashSeedList);
-
-var key = ec.keyFromPrivate(sha256DashSeedList);
+var key = functions.keyFromPrivate(sha256SeedList);
 var publicKey = key.getPublic('hex');
 var privateKey = key.getPrivate('hex');
 
-functions.outputString(dashSeedList,'red','Seed List: ',true,true);
+functions.outputString(seedList,'red','Seed List: ',true,true);
 functions.outputString(privateKey,'red','Private Key: ',true,true);
 functions.outputString(publicKey,'green','Public Key: ',true,true);
