de1d57ef17caddbf71f297e99cfd3f6c91af1134
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-23 17:45:13 -0700

    renamed to seed

--- scripts/generate/generateKeysAndSeed.js
+++ scripts/generate/generateKeysAndSeed.js
@@ -1,13 +1,13 @@
 var functions = require("../functions.js");
 
-var seedList = functions.generateSeed();
+var seed = functions.generateSeed();
 
-sha256SeedList = functions.sha256(seedList);
+sha256Seed = functions.sha256(seed);
 
-var key = functions.keyFromPrivate(sha256SeedList);
+var key = functions.keyFromPrivate(sha256Seed);
 var publicKey = key.getPublic('hex');
 var privateKey = key.getPrivate('hex');
 
-functions.outputString(seedList,'red','Seed List: ',true,true);
+functions.outputString(seed,'red','Seed: ',true,true);
 functions.outputString(privateKey,'red','Private Key: ',true,true);
 functions.outputString(publicKey,'green','Public Key: ',true,true);
--- scripts/generate/generateKeysFromSeed.js
+++ scripts/generate/generateKeysFromSeed.js
@@ -1,13 +1,13 @@
 var functions = require("../functions.js");
 var argv = require('minimist')(process.argv.slice(2));
 
-var seedList = argv.seedList;
-var sha256SeedList = functions.sha256(seedList);
+var seed = argv.seed;
+var sha256Seed = functions.sha256(seed);
 
-var key = functions.keyFromPrivate(sha256SeedList);
+var key = functions.keyFromPrivate(sha256Seed);
 var publicKey = key.getPublic('hex');
 var privateKey = key.getPrivate('hex');
 
-functions.outputString(seedList,'red','Seed List: ',true,true);
+functions.outputString(seed,'red','Seed: ',true,true);
 functions.outputString(privateKey,'red','Private Key: ',true,true);
 functions.outputString(publicKey,'green','Public Key: ',true,true);
