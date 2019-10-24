cedb5322a12da841af86b907dc70b0e92f6f188d
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-23 18:21:14 -0700

    added params

--- scripts/generate/loopOverHash.js
+++ scripts/generate/loopOverHash.js
@@ -1,15 +1,17 @@
 var functions = require("../functions.js");
+var argv = require('minimist')(process.argv.slice(2));
 
-string = 'brandon'
-instances = 10000000 // 8.7 seconds per million
+string = argv.string;
+instancesString = argv.instances;
+
+instancesInteger = parseInt(instancesString); // 8.7 seconds per million
 
 startTime = Math.floor(Date.now() / 1000) * 1000;
 
-scrambledString = functions.sha256Scramble(string,instances);
+scrambledString = functions.sha256Scramble(string,instancesInteger);
 console.log(scrambledString);
 
 endTime = Math.floor(Date.now() / 1000) * 1000;
-
 duration = (endTime - startTime) / 1000;
 
 console.log(duration);
