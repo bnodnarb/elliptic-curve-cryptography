65fb60a87133993a325a1edcff073ab648df87ac
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-25 15:49:48 -0700

    arranged

--- scripts/app.js
+++ scripts/app.js
@@ -11,14 +11,26 @@ COMMAND_SELECTED = false
 
 var argv = require('yargs')
   .demandCommand()
-  .command(['generateKeysAndSeed'], 'Create a new set of keys and corresponding seed', {}, (argv) => {
+  .command(['generateKeysAndSeed'], 'Create a new set of keys and corresponding seed\n-Requires: None, Optional: Passphrase\n', {}, (argv) => {
     runfile('generate/generateKeysAndSeed.js');
   })
-  .command(['generateKeysFromSeed'], 'Re-generate a key pair from existing seed', {}, (argv) => {
+  .command(['generateKeysFromSeed'], 'Re-generate a key pair from existing seed\n-Requires: Seed, Optional: Passphrase\n', {}, (argv) => {
     runfile('generate/generateKeysFromSeed.js');
   })
-  .command(['generateSharedKey'], 'Create a shared key for encryption/decryption', {}, (argv) => {
+  .command(['generateSharedKey'], 'Create a shared key for encryption/decryption\n-Requires: Private key, Collaborator public key\n', {}, (argv) => {
     runfile('generate/generateSharedKey.js');
+  })
+  .command(['signMessage'], 'Create a signed message\n-Requires: Private key, Message\n', {}, (argv) => {
+    runfile('msg/signMessage.js');
+  })
+  .command(['verifyMessage'], 'Verify a signed message\n-Requires: Signed message\n', {}, (argv) => {
+    runfile('msg/verifyMessage.js');
+  })
+  .command(['encryptMessage'], 'Encrypt a message\n-Requires: Shared key, Plain text message\n', {}, (argv) => {
+    runfile('msg/encryptMessage.js');
+  })
+  .command(['decryptMessage'], 'Decrypt a message\n-Requires: Shared key, Encrypted message\n', {}, (argv) => {
+    runfile('msg/decryptMessage.js');
   })
   .help('help').alias('help', 'h')
   .version(false)
