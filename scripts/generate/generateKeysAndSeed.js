31ed28f9aeafa52d818d05177007131994b5df03
bnodnarb <97063458+bnodnarb@users.noreply.github.com>
2019-10-25 15:11:31 -0700

    added passphrase

--- scripts/generate/generateKeysAndSeed.js
+++ scripts/generate/generateKeysAndSeed.js
@@ -1,13 +1,47 @@
 var functions = require("../functions.js");
+var prompt = require('prompt');
 
-var seed = functions.generateSeed();
+var schema = {
+  properties: {
+    passphrase: {
+      message: 'Your passphrase',
+      hidden: true,
+      required: false
+    },
+    reEnterPassphrase: {
+      message: 'Re-enter your passphrase',
+      hidden: true,
+      required: false
+    }
+  }
+};
 
-sha256Seed = functions.sha256(seed);
+prompt.start();
+prompt.get(schema, function (err, argv) {
+  var seed = functions.generateSeed();
+  var passphrase = argv.passphrase;
+  var reEnterPassphrase = argv.reEnterPassphrase;
 
-var key = functions.keyFromPrivate(sha256Seed);
-var publicKey = key.getPublic('hex');
-var privateKey = key.getPrivate('hex');
+  if (passphrase != reEnterPassphrase) {
+    functions.outputString('Passphrases do not match','red','',true,true);
+    process.exit()
+  }
+  if (passphrase) {
+    var sha256Seed = functions.sha256(seed + '-' + passphrase);
+  } else {
+    var sha256Seed = functions.sha256(seed);
+  }
 
-functions.outputString(seed,'red','Seed: ',true,false);
-functions.outputString(privateKey,'red','Private Key: ',false,false);
-functions.outputString(publicKey,'green','Public Key: ',false,true);
+  var key = functions.keyFromPrivate(sha256Seed);
+  var publicKey = key.getPublic('hex');
+  var privateKey = key.getPrivate('hex');
+
+  functions.outputString(seed,'red','Seed: ',true,false);
+  if (passphrase) {
+    functions.outputString('hidden','red','Passphrase: ',false,false);
+  } else {
+    functions.outputString('not set','red','Passphrase: ',false,false);
+  }
+  functions.outputString(privateKey,'red','Private Key: ',false,false);
+  functions.outputString(publicKey,'green','Public Key: ',false,true);
+});
